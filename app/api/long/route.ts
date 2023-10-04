735dc9c286556910fe7d33d9ada6b037d78df75f
Vadim Yakhin <yakhin.v@gmail.com>
2023-10-03 18:14:12 -0700

    test

--- app/api/long/route.ts
+++ app/api/long/route.ts
@@ -1,15 +1,18 @@
-function busyWait(duration: number) {
-  const end = new Date().getTime() + duration;
-  while (new Date().getTime() < end) {
-    // Busy-wait
-  }
-}
+const busyWait = (duration: number) => {
+  return new Promise((resolve) => {
+    const end = new Date().getTime() + duration;
+    while (new Date().getTime() < end) {
+      // Busy-wait
+    }
+    resolve(true);
+  });
+};
 
 // To occupy the function for 60 seconds
 
 export async function GET() {
   // sleep 50 sec
-  busyWait(10000);
+  await busyWait(10000);
 
   const data = { message: "Hello World" };
 
